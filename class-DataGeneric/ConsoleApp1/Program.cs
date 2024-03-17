821328b05b47ea8a0740fe407c8d922af8c9629c
Afzaal Isnaufal <70215760+afzaalis@users.noreply.github.com>
2024-03-17 19:40:15 +0700

    menambahkan class DataGeneric

---
+++ class-DataGeneric/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> dataString = new DataGeneric<string>("1302220104");
+        dataString.PrintData();
+
+        DataGeneric<int> dataInteger = new DataGeneric<int>(1302220104);
+        dataInteger.PrintData();
+
+        DataGeneric<double> dataDouble = new DataGeneric<double>(1302220104);
+        dataDouble.PrintData();
+    }
+}
